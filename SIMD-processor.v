e3bc706537603e84ea4e0dc6910361789abd988d
Anirudh Sridhar <aniruddh.s1217@gmail.com>
2025-03-18 23:07:12 +0530

    added basic ALU structure

---
+++ SIMD-processor.v
@@ -0,0 +1,9 @@
+module simd()
+/*
+make a mem file 
+ALU can do only MAC, for 1 word of data, try to make this floating point
+you can write another module to rearrange data
+here, optimize things for matrix multiplication, do saturation and clipping calculations
+*/
+
+endmodule
